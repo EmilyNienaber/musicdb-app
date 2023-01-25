5da634f2c7d1d63e1439f044b33836c28a2456f4
Emily <emilydutoit92@gmail.com>
2023-01-25 12:12:12 +0200

    Base style and component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Card from './Components/Card/Card';
-import './App.css';
+import Header from './Components/Header/Header';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <Card />
+    <div className={styles.App}>
+      <Header />
+      <div className={styles.contentBase}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './Card.module.css'
 
 const Card = () => {
     
     return (
-      <div>
-        Card Component
+      <div className={styles.body}>
+        <div className={styles.bodyImg} />
+        <div className={styles.infoPanel}>
+          <div className={styles.titleRow}>
+            <p className={styles.title}>On the low</p>
+            <p className={styles.trackLength}>02:44</p>
+          </div>
+          <p className={styles.artist}>by Artist Name</p>
+          <p className={styles.album}>Album Name</p>
+        </div>
       </div>
     )
 }
